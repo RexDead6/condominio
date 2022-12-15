5e4bcf177da68479e2f77416a75e4c43dc2e4b5d
RexDead6 <luis17048@gmail.com>
2022-12-14 20:05:11 -0400

    Eliminacion del Drawer

--- app/src/main/java/com/rex/condominio/activities/MainActivity.java
+++ app/src/main/java/com/rex/condominio/activities/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.rex.condominio.R;
 
@@ -15,25 +17,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_drawer);
-
-        drawer = findViewById(R.id.drawer);
-    }
-
-    public void openDrawer(){
-        drawer.openDrawer(GravityCompat.START);
-    }
-
-    public void closeDrawer(){
-        drawer.closeDrawer(GravityCompat.START);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START);
-        }else{
-            super.onBackPressed();
-        }
+        setContentView(R.layout.activity_main);
     }
 }
